db50588fc06d87da0711e37ebd94599e25f2a8b2
narwinoks <narnowin195@gmail.com>
2022-11-17 16:36:40 +0700

    fix breze and model

---
+++ database/seeds/CourirSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CourirSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
